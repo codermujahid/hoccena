5058b981127f58929efea3b7289cec2624db7f9a
mujahid <developermujahid35@gmail.com>
2022-04-17 16:40:08 +0600

    upload my assignment

---
+++ assets/app.js
@@ -0,0 +1,35 @@
+// console.log(10000)
+// console.warn('This is console.worn')
+// console.error('This is error ')
+// console.dir(6000)
+// console.info('i love Allah')
+
+
+// alert('This is Alart');
+
+// console.log(confirm('This si conformseryg'));
+// console.log(prompt('This is concole.log prompt'));
+// console.warn('tumis amar vlao kisu dekla na ');
+// console.warn('This is warn');
+// console.log('My name is mujahid');
+// console.log('maujhid ' + 12333 +1234 + 'This is dtwop'+'thert time' );
+//console.log(230-4235);
+
+
+
+// let mimi = 'kamal';
+
+// const miimi = 'muajajhid';
+
+// console.log('My name is ' + miimi +' ' + mimi +' mujahid');
+
+
+
+let mimi = prompt('your name plz');
+let age = prompt('your age plz');
+let skill = prompt('your skill plz');
+
+
+console.log(`Hi ${mimi}, you are ${age} yers old and you are a ${skill} `);
+
+
